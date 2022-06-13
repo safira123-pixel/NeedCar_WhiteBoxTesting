b566841c3d318e4b61a35be0e363de750088993c
safira123-pixel <safiraistifarini@gmail.com>
2022-06-13 23:15:03 +0700

    "penambahan fungsi"

---
+++ database/seeders/testimonialsSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class testimonialsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('testimonials')->insert([
+            'name' => 'Salsabila Natasya',
+            'email' => 'SalsabilaNat@gmail.com',
+            'car_name' => 'Mitsubisi Xpander',
+            'review' => 'Renting with affordable price in here, its so good',
+
+        ]);
+
+        DB::table('testimonials')->insert([
+            'name' => 'Bramano Aditya',
+            'email' => 'BramaKun@gmail.com',
+            'car_name' => 'Honda Brio',
+            'review' => 'Nice and comfy car',
+
+        ]);
+
+        DB::table('testimonials')->insert([
+            'name' => 'safira Istifarini',
+            'email' => 'SafiraIstifarini@gmail.com',
+            'car_name' => 'Honda Civic',
+            'review' => 'Renting car with nice experience',
+
+        ]);
+    }
+}
