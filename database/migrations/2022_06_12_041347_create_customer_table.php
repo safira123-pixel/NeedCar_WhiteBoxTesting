c95a9022e15a8436777e1b05abe9cb14a2f4efc7
Dherisma Hanindita <dherismadedek@gmail.com>
2022-06-18 16:05:54 +0700

    "Customer"

--- database/migrations/2022_06_12_041347_create_customer_table.php
+++ database/migrations/2022_06_12_041347_create_customer_table.php
@@ -19,7 +19,7 @@ class CreateCustomerTable extends Migration
             $table->string('email')->unique();
             $table->string('phone');
             $table->string('address');
-            $table->timestamps();
+            $table->timestamps(); 
         });
     }
 
