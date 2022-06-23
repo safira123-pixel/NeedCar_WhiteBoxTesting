9462d4595fdab2c32f8459a5e934a9c403a188b4
safira123-pixel <safiraistifarini@gmail.com>
2022-06-23 10:47:21 +0700

    "user edit"

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,73 +3,90 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-        $user = User::get();
-        $data = array('title' => 'User');
-        $paginate = User::orderBy('id_user', 'asc')->paginate(3);
-        return view('user.index', ['user' => $user,'paginate'=>$paginate], $data);
-    }
- public function create()
- {
-    $data = array('title' => 'User Form');
-    return view('user.create', $data);
- }
- public function store(Request $request)
- {
-    //melakukan validasi data
-    $request->validate([
-        'name' => 'required',
-        'email' => 'required',
-        'phone' => 'required',
-        'address' => 'required'
-    ]);
-    $user = new User;
-    $user->name = $request->get('name');
-    $user->email = $request->get('email');
-    $user->phone = $request->get('phone');
-    $user->address = $request->get('address');
-    $user->save();
-
-    return redirect()->route('user.index')
-    ->with('success', 'user Berhasil Ditambahkan');
- }
-
- public function edit($name)
- {
-    $data = array('title' => 'Edit User Form');
- $user = User::where('name', $name)->get();
- return view('user.edit', $data);
- }
-
- public function update(Request $request, $name)
- {
-//melakukan validasi data
- $request->validate([
- 'name' => 'required',
- 'email' => 'required',
- 'phone' => 'required',
- 'address' => 'required'
- ]);
-
- $user = User::where('name', $name)->first();
-    $user->name = $request->get('name');
-    $user->email = $request->get('email');
-    $user->phone = $request->get('phone');
-    $user->address = $request->get('address');
-    $user->save();
-
- return redirect()->route('user.index')
- ->with('success', 'user Berhasil Diupdate');
- }
-
- public function destroy( $name)
- {
- User::where('name', $name)->delete();
- return redirect()->route('user.index') -> with('success', 'Deleted User');
- }
+   public function index()
+   {
+   //fungsi eloquent menampilkan data menggunakan pagination
+   $data = array('title' => 'User Data');
+   $user = User::all(); // Mengambil semua isi tabel
+   $paginate = User::orderBy('id_user', 'asc')->paginate(3);
+   return view('user.index', ['user' => $user,'paginate'=>$paginate], $data);
+   }
+   
+   public function create()
+   {
+   $data = array('title' => 'Data Create Customer');
+   return view('user.create', $data);
+   }
+   
+   public function store(Request $request)
+   {
+   //melakukan validasi data
+   $request->validate([
+   'name' => 'required',
+   'email' => 'required',
+   'phone' => 'required',
+   'address' => 'required',
+   'status' => 'required',
+   ]);
+   //fungsi eloquent untuk menambah data
+   $user = new User;
+       $user->name = $request->get('name');
+       $user->email = $request->get('email');
+       $user->phone = $request->get('phone');
+       $user->address = $request->get('address');
+       $user->save();
+       //jika data berhasil ditambahkan, akan kembali ke halaman utama
+   return redirect()->route('user.index')
+   ->with('success', 'User Added');
+   }
+   
+   public function show($id)
+   {
+   $data = array('title' => 'Data Show Customer');
+   $User = User::where('id', $id)->first();
+   return view('user.detail', compact('User'), $data);
+   }
+   
+   public function edit($id)
+   {
+   $data = array('title' => 'Data Edit Customer');
+   $User = DB::table('user')->where('id', $id)->first();
+   return view('user.edit', compact('User'), $data);
+   }
+   
+   public function update(Request $request, $id)
+   {
+   //melakukan validasi data
+   $request->validate([
+       'name' => 'required',
+       'email' => 'required',
+       'phone' => 'required',
+       'address' => 'required',
+   ]);
+   //fungsi eloquent untuk mengupdate data inputan kita
+   $user = User::where('id', $id)->first;
+       $user->name = $request->get('name');
+       $user->email = $request->get('email');
+       $user->phone = $request->get('phone');
+       $user->address = $request->get('address');
+   
+       $user->save();
+   
+   //jika data berhasil diupdate, akan kembali ke halaman utama
+   return redirect()->route('user.index')
+   ->with('success', 'User Updated!');
+   }
+   
+   public function destroy( $id)
+   {
+   //fungsi eloquent untuk menghapus data
+   User::where('id', $id)->delete();
+   return redirect()->route('user.index')
+   -> with('success', 'User Deleted!');
+   }
 
 }
--- resources/views/user/Index.blade.php
+++ resources/views/user/Index.blade.php
@@ -2,59 +2,50 @@
 
 @section('content')
 <div class="row">
-  <div class="col-lg-12 margin-tb">
-    <div class="pull-left mt-2">
-      <h2>USER DATA</h2>
-    </div>
-    <div class="float-right my-2">
-    </div>
-  </div>
+<div class="col-lg-12 margin-tb">
+<div class="pull-left mt-2">
+<h2>User Data</h2>
+</div>
+<div class="float-right my-2">
+<!-- <a class="btn btn-success" href="admin/DataCustomer/create"> Input User</a> -->
+</div>
+</div>
 </div>
 @if ($message = Session::get('success'))
 <div class="alert alert-success">
-  <p>{{ $message }}</p>
+<p>{{ $message }}</p>
 </div>
 @endif
 @if ($message = Session::get('error'))
 <div class="alert alert-error">
-  <p>{{ $message }}</p>
+<p>{{ $message }}</p>
 </div>
 @endif
-
 <table class="table table-bordered">
-  <tr>
-    <th>Name</th>
-    <th>Email</th>
-    <th>Phone</th>
-    <th>Address</th>
-    <th>Password</th>
-    <th width="280px">Action</th>
-  </tr>
-  @if(!empty($paginate) && $paginate->count())
-    @foreach($paginate as $users)
-      <tr>
-        <td>{{ $users->name }}</td>
-        <td>{{ $users->email }}</td>
-        <td>{{ $users->phone }}</td>
-        <td>{{ $users->address}}</td>
-        <td>{{ $users->password }}</td>
-        <td>
-          <form action="{{ route('user.destroy',['user'=>$users->name]) }}" method="POST">
-            <a class="btn btn-primary" href="{{ route('user.edit',$users->name) }}">Edit</a>
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger">Delete</button>
-          </form>
-        </td>
-      </tr>
-    @endforeach
-  @else
-    <tr>
-      <td colspan="10">There are no data.</td>
-    </tr>
-  @endif
+<tr>
+<th>Name</th>
+<th>Email</th>
+<th>Phone</th>
+<th>Address</th>
+<th width="280px">Action</th>
+</tr>
+@foreach ($user as $usr)
+<tr>
+<td>{{ $usr ->name }}</td>
+<td>{{ $usr ->email }}</td>
+<td>{{ $usr ->phone }}</td>
+<td>{{ $usr ->address }}</td>
+<td>
+<!-- <a class="btn btn-info" href="/admin/DataCustomer/{id_user}/show">Show</a> -->
+<a class="btn btn-primary" href="/admin/DataCustomer/{id_user}/edit">Edit</a>
+<br>
+<form href="/admin/DataCustomer/{id_user}/destroy" method="POST">
+@csrf
+@method('DELETE')
+<button type="submit" class="btn btn-danger">Delete</button>
+</form>
+</td>
+</tr>
+@endforeach
 </table>
-
-{!! $paginate->links() !!}
-
 @endsection
