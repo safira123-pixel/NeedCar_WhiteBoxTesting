9c23ea292a9b590207b8c6b6a8c58161fa0a1a4c
Dherisma Hanindita <dherismadedek@gmail.com>
2022-06-23 11:39:09 +0700

    ContacMailNew

--- resources/views/customer/contactMail.blade.php
+++ resources/views/customer/contactMail.blade.php
@@ -4,10 +4,10 @@ You received an email from : {{ $name }} <br><br>
 
 User details: <br><br>
 
-Name:  {{ $name }}<br>
-Email:  {{ $email }}<br>
-Phone:  {{ $phone }}<br>
-Subject:  {{ $subject }}<br>
-Message:  {!! $subject !!}<br><br>
+Name    :  {{ $name }}<br>
+Email   :  {{ $email }}<br>
+Phone   :  {{ $phone }}<br>
+Subject :  {{ $subject }}<br>
+Message :  {!! $subject !!}<br><br>
 
-Thanks
+Thanks 
